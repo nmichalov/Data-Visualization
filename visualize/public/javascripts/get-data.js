fa7b091cd8e1b5daaab6d795851dbbde1dee3b27
Nathan Michalov <nmichalov@gmail.com>
2012-03-23 15:40:13 -0400

    shows URLs in browser

--- visualize/public/javascripts/get-data.js
+++ visualize/public/javascripts/get-data.js
@@ -4,7 +4,7 @@ $.getJSON(topicURL, function(data) {
     console.log('get json called');
     $.each(data, function(key, val) {
         console.log('key '+key+' val '+val); 
-        $("body").append(val.url);
+        $("body").append(val.url+'<br>');
     });
   });
 
